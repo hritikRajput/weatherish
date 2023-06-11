455a51cb773af837ee41c19f22d18db25d896366
hritikRajput <hritikshivam@gmail.com>
2023-06-11 23:22:46 +0530

    Add functions and icons for extratempinfo

--- src/UI.js
+++ src/UI.js
@@ -1,5 +1,9 @@
 let currentWeatherContainer = document.querySelector(".currentWeatherContainer")
 let hourlyWeatherContainer = document.querySelector(".hourlyWeatherContainer")
+import thermoImg from './assets/thermo.svg';
+import humidityImg from './assets/humidity.svg';
+import rainyImg from './assets/rainy.svg';
+import windImg from './assets/wind.svg';
 
 function updateWeatherUI(weatherData){
     console.log(weatherData)
@@ -51,23 +55,113 @@ function createTempInfo(condition, location, time, temp, mintemp, maxtemp){
 function createExtraInfo(feelsLike, humidity, visibility, windSpeed){
     const extraInfoContainer = document.createElement("div");
     extraInfoContainer.className = "extraInfoContainer"
-    const feelsLikeEl = document.createElement("p");
-    const humidityEl = document.createElement("p");
-    const visibilityEl = document.createElement("p");
-    const windSpeedEl = document.createElement("p");
+    const thermoContainer = createThermoContainer(feelsLike);
+    const humidityContainer = createHumidityContainer(humidity);
+    const visibilityContainer = createVisibilityContainer(visibility);
+    const windContainer = createWindContainer(windSpeed);
 
-    feelsLikeEl.textContent = `${feelsLike} \xB0C`;
-    humidityEl.textContent = `${humidity} \u0025`;
-    visibilityEl.textContent= `${visibility} \u0025`;
-    windSpeedEl.textContent= `${windSpeed} km/h`;
-
-    extraInfoContainer.append(feelsLikeEl)
-    extraInfoContainer.append(humidityEl)
-    extraInfoContainer.append(visibilityEl)
-    extraInfoContainer.append(windSpeedEl)
+    extraInfoContainer.append(thermoContainer)
+    extraInfoContainer.append(humidityContainer)
+    extraInfoContainer.append(visibilityContainer)
+    extraInfoContainer.append(windContainer)
 
     return extraInfoContainer;
 }
+
+function createThermoContainer(feelsLike){
+    const thermoContainer = document.createElement("div")
+    thermoContainer.classList.add("thermo-container")
+    const thermoImage = document.createElement("div")
+    thermoImage.classList.add("thermo-image")
+    const thermoContent = document.createElement("div")
+    thermoContent.classList.add("thermo-content")
+
+    const img = document.createElement("img")
+    img.src=thermoImg;
+
+    const p1 = document.createElement("p");
+    p1.textContent = "Feels like"
+    const p2 = document.createElement("p");
+    p2.textContent = `${feelsLike} \xB0C`;
+
+    thermoImage.append(img)
+    thermoContent.append(p1)
+    thermoContent.append(p2)
+    thermoContainer.append(thermoImage)
+    thermoContainer.append(thermoContent)
+    return thermoContainer
+}
+function createHumidityContainer(humidity){
+    const humidityContainer = document.createElement("div")
+    humidityContainer.classList.add("humidity-container")
+    const humidityImage = document.createElement("div")
+    humidityImage.classList.add("humidity-image")
+    const humidityContent = document.createElement("div")
+    humidityContent.classList.add("humidity-content")
+
+    const img = document.createElement("img")
+    img.src=humidityImg;
+
+    const p1 = document.createElement("p");
+    p1.textContent = "Humidity"
+    const p2 = document.createElement("p");
+    p2.textContent = `${humidity} \u0025`;
+
+    humidityImage.append(img)
+    humidityContent.append(p1)
+    humidityContent.append(p2)
+    humidityContainer.append(humidityImage)
+    humidityContainer.append(humidityContent)
+    return humidityContainer
+}
+
+function createVisibilityContainer(visibility){
+    const visibilityContainer = document.createElement("div")
+    visibilityContainer.classList.add("visibility-container")
+    const visibilityImage = document.createElement("div")
+    visibilityImage.classList.add("visibility-image")
+    const visibilityContent = document.createElement("div")
+    visibilityContent.classList.add("visibility-content")
+
+    const img = document.createElement("img")
+    img.src=rainyImg;
+
+    const p1 = document.createElement("p");
+    p1.textContent = "visibility"
+    const p2 = document.createElement("p");
+    p2.textContent = `${visibility} \u0025`;
+
+    visibilityImage.append(img)
+    visibilityContent.append(p1)
+    visibilityContent.append(p2)
+    visibilityContainer.append(visibilityImage)
+    visibilityContainer.append(visibilityContent)
+    return visibilityContainer
+}
+function createWindContainer(wind){
+    const windContainer = document.createElement("div")
+    windContainer.classList.add("wind-container")
+    const windImage = document.createElement("div")
+    windImage.classList.add("wind-image")
+    const windContent = document.createElement("div")
+    windContent.classList.add("wind-content")
+
+    const img = document.createElement("img")
+    img.src=windImg;
+
+    const p1 = document.createElement("p");
+    p1.textContent = "wind"
+    const p2 = document.createElement("p");
+    p2.textContent = `${wind} km/h`;
+
+    windImage.append(img)
+    windContent.append(p1)
+    windContent.append(p2)
+    windContainer.append(windImage)
+    windContainer.append(windContent)
+    return windContainer
+}
+
 
 function createHourlyInfo(hour){
     const hourlyInfoContainer = document.createElement("div");
