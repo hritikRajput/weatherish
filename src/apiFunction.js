eb770d0af80511494189e76b8ed4a9a92d98377f
hritikRajput <hritikshivam@gmail.com>
2023-06-13 08:45:22 +0530

    Modify the url to https in getForecast function in apiFunction.js

--- src/apiFunction.js
+++ src/apiFunction.js
@@ -1,6 +1,6 @@
 async function getForecast(location){
     
-    let url = `http://api.weatherapi.com/v1/forecast.json?key=be85db4c9bfc4cb9b50172920230306&q=${location}&days=2`
+    let url = `https://api.weatherapi.com/v1/forecast.json?key=be85db4c9bfc4cb9b50172920230306&q=${location}&days=2`
     let response = await fetch(url)
     let forecastData = await response.json()
     console.log(forecastData)
